dcf16f5364cd2a4ad8c6150eb4a3883f27bd5f55
AlexSJC <cn_sjc@outlook.com>
2024-03-01 22:42:53 +0800

    Finish.

--- Wildcard/Wildcard.cs
+++ Wildcard/Wildcard.cs
@@ -1,6 +1,25 @@
-﻿namespace Asjc.Wildcard
+﻿using System.Text.RegularExpressions;
+
+namespace Asjc.Wildcard
 {
     public class Wildcard
     {
+        public Wildcard(string pattern) => Pattern = pattern;
+
+        public string Pattern { get; }
+
+        public bool IsMatch(string input)
+            => IsMatch(input, Pattern);
+
+        public static bool IsMatch(string input, string pattern)
+            => Regex.IsMatch(input, ToRegex(pattern));
+
+        public string ToRegex()
+            => ToRegex(Pattern);
+
+        public static string ToRegex(string pattern)
+            => $"^{Regex.Escape(pattern).Replace("\\*", ".*").Replace("\\?", ".")}$";
+
+        public override string ToString() => Pattern;
     }
 }
