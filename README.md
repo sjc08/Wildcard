812f0fd889f7f6d322711c3a5380c5ca8717634e
AlexSJC <cn_sjc@outlook.com>
2024-03-02 23:06:23 +0800

    Update.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-Match strings with wildcard.
+[![NuGet](https://img.shields.io/nuget/v/Asjc.Wildcard)](https://www.nuget.org/packages/Asjc.Wildcard/)
+
+A very simple package that implements **wildcard** matching. Don't expect it to be fast, because its essential operation is to convert to `regex`. Therefore, its only purpose is to simplify the input, not replace the regex.
+
+## Quick Start
+
+```c#
+new Wildcard("Does * work\\?").IsMatch("Does it work?");
+```
 
--- Wildcard/Wildcard.cs
+++ Wildcard/Wildcard.cs
@@ -1,24 +1,63 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Asjc.Wildcard
 {
     public class Wildcard
     {
-        public Wildcard(string pattern) => Pattern = pattern;
+        public Wildcard(string pattern)
+        {
+            Pattern = pattern;
+            EquivalentRegex = ToRegex(pattern);
+        }
 
         public string Pattern { get; }
+        public string EquivalentRegex { get; }
 
         public bool IsMatch(string input)
-            => IsMatch(input, Pattern);
+            => Regex.IsMatch(input, EquivalentRegex);
 
         public static bool IsMatch(string input, string pattern)
-            => Regex.IsMatch(input, ToRegex(pattern));
-
-        public string ToRegex()
-            => ToRegex(Pattern);
+            => new Wildcard(pattern).IsMatch(input);
 
         public static string ToRegex(string pattern)
-            => $"^{Regex.Escape(pattern).Replace("\\*", ".*").Replace("\\?", ".")}$";
+        {
+            var sb = new StringBuilder();
+            bool escaping = false;
+
+            sb.Append("^");
+
+            foreach (var c in pattern)
+            {
+                if (escaping)
+                {
+                    sb.Append(Regex.Escape(c.ToString()));
+                    escaping = false;
+                }
+                else
+                {
+                    switch (c)
+                    {
+                        case '\\':
+                            escaping = true;
+                            break;
+                        case '*':
+                            sb.Append(".*");
+                            break;
+                        case '?':
+                            sb.Append(".");
+                            break;
+                        default:
+                            sb.Append(Regex.Escape(c.ToString()));
+                            break;
+                    }
+                }
+            }
+
+            sb.Append("$");
+
+            return sb.ToString();
+        }
 
         public override string ToString() => Pattern;
     }
